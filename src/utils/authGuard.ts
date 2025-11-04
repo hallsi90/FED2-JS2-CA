b686bfa30e20bf289bf45834350d304bb5ff14ee
Ingelinn Hallseth <ingelinn@hotmail.com>
2025-11-04 18:18:46 +0100

    feat: add auth guard to protect pages. Closes #12

--- src/utils/authGuard.ts
+++ src/utils/authGuard.ts
@@ -1,1 +1,19 @@
+// authGuard.ts
 // check if logged in, maybe redirect if not
+
+import { getToken } from "../api/storage";
+
+/**
+ * Check if the user is logged in.
+ * If there is no token in localStorage, redirect to login page.
+ * Import this function to any page that require login (like create/edit/delete post)
+ */
+export function requireAuth() {
+  const token = getToken();
+
+  // If no token, redirect to login page
+  if (!token) {
+    window.location.href = "/login/";
+    return;
+  }
+}
